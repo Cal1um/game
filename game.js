df8375877cc542aa0cfa7c3e706f7d31af77fbc1
Cal1um <98314842+Cal1um@users.noreply.github.com>
2022-11-07 11:54:18 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -20,45 +20,72 @@ export default class Game{
 
         this.RightLeft = 5
         this.UpDown = 5
-
-       
+        
+        this.layer = []
+        
     }
-
+    
     newlevel(){
-        console.log(map[5][7])
+
+        this.random = Math.floor(Math.random() * 6) + 1;
+        if(this.firstloop === 0){
+            
+            for (var i = 0; i < 11; i++){
+                this.layer[i] = [];
+                for (var j = 0; j < 11; j++){
+                    this.layer[i][j] = Math.floor(Math.random() * 6) + 1;
+    
+                }
+            }
+            this.layer[5][5] = 0
+            console.log(this.layer)
+        }
+        
+        
+        this.firstloop = 1
+
+        this.currentLevel = this.layer[this.RightLeft][this.UpDown]
 
         if(this.player.newlevel === 1){ //right door
-            this.room = map[this.RightLeft][this.UpDown]
+            this.RightLeft += 1
+            this.room = this.layer[this.RightLeft][this.UpDown]
             this.currentLevel = this.room
 
             console.log(this.player.newlevel)
             this.player.position.x = this.gameWidth - this.player.width
+            this.player.newlevel = 0
         }
 
         if(this.player.newlevel === 2){ //left door
-            this.room = map[this.RightLeft][this.UpDown]
+            this.RightLeft -= 1
+            this.room = this.layer[this.RightLeft][this.UpDown]
             this.currentLevel = this.room
 
             console.log(this.player.newlevel)
             this.player.position.x = 0
+            this.player.newlevel = 0
         }
 
         if(this.player.newlevel === 3){ //bottum door
-            this.room = map[this.RightLeft][this.UpDown]
+            this.UpDown += 1
+            this.room = this.layer[this.RightLeft][this.UpDown]
             this.currentLevel = this.room
 
             
             console.log(this.player.newlevel)
             this.player.position.y = this.gameHeight - this.player.height
+            this.player.newlevel = 0
         }
 
         if(this.player.newlevel === 4){ //top door
-            this.room = map[this.RightLeft][this.UpDown]
+            this.UpDown -= 1
+            this.room = this.layer[this.RightLeft][this.UpDown]
             this.currentLevel = this.room
 
 
             console.log(this.player.newlevel)
             this.player.position.y = 0
+            this.player.newlevel = 0
         }
         
         this.levels = [level0, level1, level2, level3, level4, level5, level6]
@@ -73,7 +100,6 @@ export default class Game{
     }
 
     start(){
-        this.random = Math.floor(Math.random() * 6) + 1;
         this.player = new Player(this, this.currentLevel);
 
         this.projectileup = new ProjectileUp(this);
@@ -83,22 +109,10 @@ export default class Game{
 
         this.projectiledely = new ProjectileDely(this);
 
-        var map = [];
-        for (var i = 0; i < 10; i++){
-            map[i] = [];
-            for (var j = 0; j < 10; j++){
-                map[i][j] = Math.floor(Math.random() * 6) + 1;
-                
-            }
+        if(this.firstloop === 0){
+            this.newlevel()            
         }
-        
-        map[5][5] = 0
-
-        
-        console.log(map[5][7])
-        
-
-        this.currentLevel = map[this.RightLeft][this.UpDown]
+        this.firstloop = 1      
 
         this.levels = [level0, level1, level2, level3, level4, level5, level6]
 
@@ -108,8 +122,33 @@ export default class Game{
 
         
         this.gameObjects = [...tile, this.projectileup, this.projectiledown, this.projectileleft, this.projectileright, this.player, this.projectiledely];
+
+
+
+        
         
     }
+
+    /*layergen(){
+        this.random = Math.floor(Math.random() * 6) + 1;
+
+
+        var layer = [];
+        for (var i = 0; i < 11; i++){
+            layer[i] = [];
+            for (var j = 0; j < 11; j++){
+                layer[i][j] = Math.floor(Math.random() * 6) + 1;
+                
+            }
+        }
+        
+        layer[5][5] = 0
+        
+
+        this.currentLevel = layer[this.RightLeft][this.UpDown]
+
+        this.newlevel(layer);
+    }*/
 
     
 
